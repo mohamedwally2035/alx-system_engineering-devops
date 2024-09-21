7516703f8390c58af40821102e60359d1599d611
mohamedwally2035 <mohamedwally87@gmail.com>
2024-09-21 06:03:49 -0700

    adit4

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,27 +1,40 @@
 #!/usr/bin/python3
-"""
-queries the Reddit API and prints the titles of
-the first 10 hot posts listed for a given subreddit.
-"""
+"""Module that consumes the Reddit API and prints the titles of the first
+10 hot posts listed for a given subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    """
-    prints the titles of the first 10 hot posts listed for
-    a given subreddit
-    """
-    url = ("https://api.reddit.com/r/{}?sort=hot&limit=10".format(subreddit))
-    headers = {'User-Agent': 'CustomClient/1.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    """Queries the Reddit API and prints the titles of the first 10 hot
+    posts listed for a given subreddit.
 
-    if response.status_code != 200:
-        print(None)
-        return
-    response = response.json()
-    if 'data' in response:
-        for posts in response.get('data').get('children'):
-            print(posts.get('data').get('title'))
+    If not a valid subreddit, print None.
+    Invalid subreddits may return a redirect to search results. Ensure
+    that you are not following redirects.
 
+    Args:
+        subreddit (str): subreddit
+
+    Returns:
+        str: titles of the first 10 hot posts
+    """
+    base_url = 'https://www.reddit.com'
+    sort = 'top'
+    limit = 10
+    url = '{}/r/{}/.json?sort={}&limit={}'.format(
+        base_url, subreddit, sort, limit)
+    headers = {
+        'User-Agent':
+        'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
+        Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
+    }
+    response = requests.get(
+        url,
+        headers=headers,
+        allow_redirects=False
+    )
+    if response.status_code == 200:
+        for post in response.json()['data']['children'][0:10]:
+            print(post['data']['title'])
     else:
         print(None)
